bb0fc99bc4be5fd770b23edaf524b269b8d9a415
daryna.kaloshyna <daryna.kaloshyna@nure.ua>
2019-11-17 10:03:07 +0200

    Implemented FindAll () method

--- UM/src/test/java/UM/kaloshyna/nure/db/HsqldbUserDaoTest.java
+++ UM/src/test/java/UM/kaloshyna/nure/db/HsqldbUserDaoTest.java
@@ -2,10 +2,17 @@ package UM.kaloshyna.nure.db;
 
 import java.util.Date;
 
+import org.dbunit.DatabaseTestCase;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.XmlDataSet;
+
+
 import UM.kaloshyna.nure.User;
 import junit.framework.TestCase;
 
-public class HsqldbUserDaoTest extends TestCase {
+public class HsqldbUserDaoTest extends DatabaseTestCase {
 	
 	private HsqldbUserDao dao;
 	private ConnectionFactory connectionFactory;
@@ -14,7 +21,6 @@ public class HsqldbUserDaoTest extends TestCase {
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		connectionFactory = new ConnectionFactoryImpl();
 		dao = new HsqldbUserDao (connectionFactory);
 	}
 
@@ -35,6 +41,31 @@ public class HsqldbUserDaoTest extends TestCase {
 			fail(e.toString());
 		}
 		
+	}
+	
+	public void testFindAll() {
+		try {
+			java.util.Collection collection = dao.findAll();
+            assertNotNull(collection);
+            assertEquals(2, collection.size());
+		} catch (DatabaseException e) {
+			e.printStackTrace();
+			fail(e.toString());
+		}
+	}
+
+
+	@Override
+	protected IDatabaseConnection getConnection() throws Exception {
+		connectionFactory = new ConnectionFactoryImpl();
+		return new DatabaseConnection(connectionFactory.createConnection());
+	}
+
+
+	@Override
+	protected IDataSet getDataSet() throws Exception {
+		IDataSet dataSet = new XmlDataSet(getClass().getClassLoader().getResourceAsStream("usersDataSet.xml"));
+		return dataSet;
 	}
 
 }
