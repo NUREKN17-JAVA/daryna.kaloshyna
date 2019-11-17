0e7b00187e7397002099c593369b90ba16c6e756
daryna.kaloshyna <daryna.kaloshyna@nure.ua>
2019-11-17 10:55:47 +0200

    Fully implemented getUserDao method

--- UM/src/main/java/UM/kaloshyna/nure/db/ConnectionFactoryImpl.java
+++ UM/src/main/java/UM/kaloshyna/nure/db/ConnectionFactoryImpl.java
@@ -5,13 +5,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactoryImpl implements ConnectionFactory {
+	
+	private String driver;
+	private String url;
+	private String user;
+	private String password;
+
+	public ConnectionFactoryImpl(String driver, String url, String user, String password) {
+		this.driver = driver;
+		this.url = url;
+		this.user = user;
+		this.password = password;
+	}
+
+	
 
 	@Override
 	public Connection createConnection() throws DatabaseException {
-		String url = "jdbc:hsqldb:file:db/UM";
-		String user = "sa";
-		String password = "";
-		String driver = "org.hsqldb.jdbcDriver";
+		
 		try {
 			Class.forName(driver);
 		} catch (ClassNotFoundException e) {
---
+++ UM/src/main/java/UM/kaloshyna/nure/db/DaoFactory.java
@@ -0,0 +1,40 @@
+package UM.kaloshyna.nure.db;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class DaoFactory {
+	
+	private static final String USER_DAO = "dao.UM.kaloshyna.nure.db.UserDao";
+	private final Properties properties;
+	
+	public DaoFactory() {
+		properties = new Properties();
+		try {
+			properties.load(getClass().getClassLoader().getResourceAsStream("settings.properties"));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private ConnectionFactory getConnectionFactory() {
+		String user = properties.getProperty("connection.user");
+		String password = properties.getProperty("connection.password");
+		String url = properties.getProperty("connection.url");
+		String driver = properties.getProperty("connection.driver");
+		
+		return new ConnectionFactoryImpl(driver, url, user, password);
+	}
+	
+	public UserDao getUserDao() {
+		UserDao result = null;
+		try {
+			Class clazz = Class.forName(properties.getProperty(USER_DAO));
+			UserDao userDao = (UserDao) clazz.newInstance();
+			userDao.setConnectionFactory(getConnectionFactory());
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return result;
+	}
+}
